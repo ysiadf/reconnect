ff41f207c82a597e5aef0023e7579cbc3965faed
Ysiad Ferreiras <ysiadf@gmail.com>
2014-06-12 18:34:18 -0700

    Add basic Google Oauth2
    Refer to steps on:
    http://richonrails.com/articles/google-authentication-in-ruby-on-rails#.
    U5pU0ZRdUSR
    and make sure to turn on the Contacts API and the Google+ apis in the
    google developers console

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: "home#show"
+
+  #get 'sessions/create'
+  #get 'sessions/destroy'
+  #get 'home/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
